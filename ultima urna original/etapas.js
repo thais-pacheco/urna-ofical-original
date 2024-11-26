d6711aa92bbc0d7c39b77c5e07c6349e9d9894ab
thais-pacheco <thais-pacheco@educar.rs.gov.br>
2024-11-25 23:41:46 -0300

    Add files via upload

---
+++ ultima urna original/etapas.js
@@ -0,0 +1,86 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '11444',
+                nome: 'Sabrina Santos',
+                partido: 'ABC',
+                fotos:[
+                    {url: '38111.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '22355',
+                nome: 'Lucas Beneti',
+                partido: 'DEFG',
+                fotos:[
+                    {url: '77222.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '13544',
+                nome: 'Luiza Lopez',
+                partido: 'HIGK',
+                fotos:[
+                    {url: '77222.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '11211',
+                nome: 'Carla Farias',
+                partido: 'LMNO',
+                fotos:[
+                    {url: '77222.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '23122',
+                nome: 'Luciano Klein',
+                partido: 'LMNO',
+                fotos:[
+                    {url: '77222.png', legenda: 'Vereador'}
+                ]
+            },
+            
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Alex Almeida',
+                partido: 'ABC',
+                vice: 'Metamao Lopes',
+                fotos:[
+                    {url: '99.png', legenda: 'Prefeito'},
+                    {url: '99_2.png', legenda: 'Vice-Prefeito', small:true}
+                ]
+
+            },
+            {
+                numero: '84',
+                nome: 'Maria Oliveira ',
+                partido: 'QWERTY',
+                vice: 'Falsilane Amarrado',
+                fotos:[
+                    {url: '84.png', legenda: 'Prefeito'},
+                    {url: '84_2.png', legenda: 'Vice-Prefeito', small:true}
+                    ]
+            },
+            {
+                numero: '77',
+                nome: 'joao silva',
+                partido: 'WHVRTX',
+                vice: 'querida nova',
+                fotos:[
+                    {url: '77.png', legenda: 'Prefeito'},
+                    {url: '77_2.png', legenda: 'Vice-Prefeito', small:true}
+                    ]
+            },
+        ]
+    }
+]
